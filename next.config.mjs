94a4703dd32dfc1f00e56bae0cd891bc1aa91685
Sahil Niraula <Sahil Niraula>
2025-08-25 07:50:02 +0545

    chore: metadata fixing

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,21 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "afitravelandtourism.com", // non-www
+          },
+        ],
+        destination: "https://www.afitravelandtourism.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
